2100895bd807c78a06294b2b550d8663c50679e9
Kamil Krauze <kamila.p.krauze@gmail.com>
2021-11-11 11:41:13 +0000

    Created menu display

---
+++ menu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void displayMenu()
+{
+	printf("========================================\n");
+	printf("\t\tWELCOME !\n");
+	printf("========================================\n\n");
+	printf("\t1. - ")
+}
+
+int main()
+{
+	displayMenu();
+	return 0;
+}
