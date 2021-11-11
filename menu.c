5931690a00b58a563134be9666818b9d317abff8
Kamil Krauze <kamila.p.krauze@gmail.com>
2021-11-11 11:54:17 +0000

    Update #1

--- menu.c
+++ menu.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <unistd.h> // Add sleep function into program - 
+
+#include "system.h"
 
 void displayMenu()
 {
 	printf("========================================\n");
 	printf("\t\tWELCOME !\n");
 	printf("========================================\n\n");
-	printf("\t1. - ")
+	printf("\t1. - Create 1D Grid\n");
+	printf("\t2. - Create 2D Grid\n");
 }
 
 int main()
 {
 	displayMenu();
+	sleep(0.5);
+	clear();
 	return 0;
 }
