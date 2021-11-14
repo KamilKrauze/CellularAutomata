f26e0e5c985420d9506ab4e53581fb83f69bd469
Inglish123 <72400652+Inglish123@users.noreply.github.com>
2021-11-14 21:50:41 +0000

    Add files via upload
    Reading and writing from file

---
+++ IO.h
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+#define SUCCESS 100
+#define INVALID_INPUT_PARAMETER 101
+
+#define PARTIAL_SUCCESS 102
+
+#define MEMORY_ALLOCATION_ERROR 103
+
+#define NOT_IMPLEMENTED 104
+#define FILE_IO_ERROR 105
+
+#define OUT_OF_BOUNDS 'Z'
+#define EMPTY_CELL '0'
+#define FULL_CELL '1'
