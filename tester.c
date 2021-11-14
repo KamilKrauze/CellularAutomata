d585982e7b47ad630060d55e0d6200f9fd944f2a
KamilKrauze <kamila.p.krauze@gmail.com>
2021-11-14 19:19:01 +0000

    Alteration and main.c as main src file

--- tester.c
+++ tester.c
@@ -1,33 +1,40 @@
-#include "grid.h"
-#include <stdio.h>
-#include <stdlib.h>
-// Add sleep function into program - https://stackoverflow.     com/questions/14818084/what-is-the-proper-include-for-the-function-sleep - 11/11/2021
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-#include "system.h"
-bool ruleArray[] = {1, 0, 0, 0, 0, 0, 0, 0};
+// #include "grid.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// // Add sleep function into program - https://stackoverflow.     com/questions/14818084/what-is-the-proper-include-for-the-function-sleep - 11/11/2021
+// #ifdef _WIN32
+// #include <Windows.h>
+// #else
+// #include <unistd.h>
+// #endif
+// #include "system.h"
+// bool ruleArray[] = {1, 0, 0, 0, 0, 0, 0, 0};
 
-extern int grid1dColCount;
-extern int grid2dColCount;
-extern int grid2dRowCount;
+// extern int grid1dColCount;
+// extern int grid2dColCount;
+// extern int grid2dRowCount;
 
-int main()
-{
-    grid2dColCount = 10;
-    grid2dRowCount = 10;
+// int main()
+// {
+//     grid2dColCount = 10;
+//     grid2dRowCount = 10;
 
-    Grid2D *gridPtr = initialize2DGrid(EMPTY_CELL);
+//     Grid2D *gridPtr = initialize2DGrid(EMPTY_CELL);
 
-    display2DGrid(gridPtr);
-    updateGrid2D(gridPtr, 5, 5, FULL_CELL);
-    updateGrid2D(gridPtr, 6, 5, FULL_CELL);
-    updateGrid2D(gridPtr, 6, 7, FULL_CELL);
-    updateGrid2D(gridPtr, 6, 6, FULL_CELL);
-    updateGrid2D(gridPtr, 5, 6, FULL_CELL);
-    display2DGrid(gridPtr);
+//     display2DGrid(gridPtr);
+//     updateGrid2D(gridPtr, 5, 5, FULL_CELL);
+//     updateGrid2D(gridPtr, 6, 5, FULL_CELL);
+//     updateGrid2D(gridPtr, 6, 7, FULL_CELL);
+//     updateGrid2D(gridPtr, 6, 6, FULL_CELL);
+//     updateGrid2D(gridPtr, 5, 6, FULL_CELL);
+//     display2DGrid(gridPtr);
+
+
+//     runConwaysGameOfLife(gridPtr, 3, false);
+
+//     free2DGrid(gridPtr);
+//     return 0;
+// }
 
     // runConwaysGameOfLife(gridPtr, 1, false);
 
@@ -44,8 +51,3 @@ int main()
     // Cell cell;
     // getValueGrid2D(gridPtr, &cell, 0, 0);
     // printf(" Result: %c\n", cell);
-    runConwaysGameOfLife(gridPtr, 3, false);
-
-    free2DGrid(gridPtr);
-    return 0;
-}
